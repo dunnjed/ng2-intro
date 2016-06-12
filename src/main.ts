238a8348f9b0dbf8038357e65213b805d9db9d3c
JD <none>
2016-06-11 22:39:01 -0400

    Removed code for firebase from index.html to main.ts file. Saved firebase as a dependency in the package json.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,41 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent } from './app/app.component';
+let firebase = require('firebase');
+
 if (process.env.ENV === 'production') {
   enableProdMode();
 }
-bootstrap(AppComponent, []);
+
+
+// Initialize Firebase
+let config = {
+  apiKey: "AIzaSyBumYG81Ird0MHFgMg12txHnoLgrKd-yT8",
+  authDomain: "ng2-intro.firebaseapp.com",
+  databaseURL: "https://ng2-intro.firebaseio.com",
+  storageBucket: "",
+};
+firebase.initializeApp(config);
+
+let provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth().signInWithPopup(provider).then(function (result: any) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  let token = result.credential.accessToken;
+  // The signed-in user info.
+  let user = result.user;
+
+  console.log(user);
+  bootstrap(AppComponent, []);
+  // ...
+}).catch(function (error: any) {
+  // Handle Errors here.
+  let errorCode = error.code;
+  let errorMessage = error.message;
+  // The email of the user's account used.
+  let email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  let credential = error.credential;
+  // ...
+});
+
+
