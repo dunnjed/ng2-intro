0fe4ca244fec4f6094aaa1c362962c1170a73a6b
JD <none>
2016-06-14 00:04:51 -0400

    Incorparted the dashboard route.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,35 @@
 import { Component } from '@angular/core';
 import { HeroesComponent } from './heroes.component';
+import {DashboardComponent} from './dashboard.component';
 import { HeroService } from './hero.service';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router-deprecated';
 
 @Component({
     selector: 'my-app',
     template: `
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <nav>
+        <a [routerLink]="['Dashboard']">Dashboard</a>
+        <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
     `,
-    directives: [HeroesComponent],
-    providers: [HeroService]
+    directives: [ROUTER_DIRECTIVES],
+    providers: [ROUTER_PROVIDERS, HeroService]
 })
+@RouteConfig([
+    {
+        path: '/heroes',
+        name: 'Heroes',
+        component: HeroesComponent
+    },
+    {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+    }
+])
 export class AppComponent {
     title: string = "Tour of Heroes";
 }
