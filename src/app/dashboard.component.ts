220432c6cec54c5ba23c0e69d2ede33acc32ddb7
JD <none>
2016-06-15 22:35:54 -0400

    Added the constructor, the ngOnInit and gotoDetail functions in the dashboard component. Added the heroes property as well.

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,9 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from './hero';
 import {HeroService} from './hero.service';
+import {Router} from '@angular/router-deprecated';
 
 @Component({
   selector: 'my-dashboard',
   template: require('./dashboard.component.html')
 })
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService,
+    private router: Router) {
+
+  }
+
+  ngOnInit() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5));
+  }
+
+  gotoDetail(hero: Hero) {
+    let link = ['HeroDetail', {id: hero.id}];
+    this.router.navigate(link);
+  }
+}
