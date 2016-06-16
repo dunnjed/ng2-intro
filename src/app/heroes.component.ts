7549afd1640c888bcf43e23da30ad81354384eb8
JD <none>
2016-06-15 22:49:01 -0400

    Updated heroes component to use the Router service and implemented the gotoDetail function.

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { HeroDetailComponent } from './hero-detail.component';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import {Router} from '@angular/router-deprecated';
 
 @Component({
   selector: 'my-heroes',
   template: require('./heroes.component.html'),
   styles: [require('./heroes.component.css')],
-  directives: [HeroDetailComponent],
+  directives: [],
   providers: []
 })
 export class HeroesComponent implements OnInit {
@@ -17,7 +18,11 @@ export class HeroesComponent implements OnInit {
 
   public heroes: Hero[];
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService,
+    private router: Router) {
+
+    }
 
   ngOnInit() {
     this.getHeroes();
@@ -29,6 +34,10 @@ export class HeroesComponent implements OnInit {
 
   getHeroes() {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  gotoDetail() {
+    this.router.navigate(['HeroDetail', {id: this.selectedHero.id}]);
   }
 
 
